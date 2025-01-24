43ffdf46a42a47dc023c196118e6d0a576883aae
Hady <itshadyy@gmail.com>
2025-01-24 14:47:30 +0200

    Initial commit

---
+++ src/App 2.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Solutions from './pages/Solutions';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Quote from './pages/Quote';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/solutions" element={<Solutions />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
